d08b06da13986b9c050986e692dcc40256a28b49
wanglin <wanglin46@lenovo.com>
2020-09-22 18:49:24 +0800

    a

--- 1.Promise/1.高阶函数/1.high-order.js
+++ 1.Promise/1.高阶函数/1.high-order.js
@@ -11,3 +11,19 @@
   *  3. instanceof: 判断谁是谁的实例 .__proto__
   *  4. Object.prototype.toString.call
   */
+
+function isType (type) {
+  // 将string保存在这个代码块中
+  return function (content) {
+    return Object.prototype.toString.call(content) === `[object ${type}]`
+  }
+}
+
+// 高阶函数实现了第一个功能 保存变量(闭包)
+let isString = isType('String')
+console.log(isString('hello'))
+
+/**
+ * 什么叫闭包：
+ *  在定义的时候，就决定了函数的作用域在哪，一个函数不在自己所在作用域下执行，可以称为闭包
+ */
